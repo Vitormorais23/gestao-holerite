2541cb3ed893d2209009a7a96221385173e236e9
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-09-24 17:42:21 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadFilesComponent } from './upload-files/upload-files.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+    children: [
+      { path: '', component: UploadFilesComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from './layout/app.layout.module';
+import { UploadFilesComponent } from './upload-files/upload-files.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
