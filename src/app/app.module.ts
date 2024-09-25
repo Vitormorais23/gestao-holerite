b1b798ad1f7a37fc1c68c2eda466bcbee7c5369b
josevitornm2015 <josevitornm2015@gmail.com>
2024-09-25 16:45:54 +0000

    instalação do firebase cloud storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { UploadFilesModule } from './upload-files/upload-files.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     UploadFilesModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"gestao-de-holerite","appId":"1:739116739972:web:d9de3cdac9f936355e01d1","storageBucket":"gestao-de-holerite.appspot.com","locationId":"southamerica-east1","apiKey":"AIzaSyBdmfYajolV6BtfGoBbErX-Hrjupd_vom0","authDomain":"gestao-de-holerite.firebaseapp.com","messagingSenderId":"739116739972","measurementId":"G-BE724DMCE0"})),
+    provideStorage(() => getStorage())
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
