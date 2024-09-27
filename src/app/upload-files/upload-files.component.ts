b867806323249d61bc2f46fc22fd1304304e5d5f
josevitornm2015 <josevitornm2015@gmail.com>
2024-09-27 20:46:45 +0000

    Alterações no código de upload, e comentando o app-config

--- src/app/upload-files/upload-files.component.ts
+++ src/app/upload-files/upload-files.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { uploadBytes, Storage } from '@angular/fire/storage';
+import { Storage } from '@angular/fire/storage';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { Message, MessageService } from 'primeng/api';
 import { MessagesModule } from 'primeng/messages';
@@ -15,67 +15,68 @@ import { FileUploadHandlerEvent } from 'primeng/fileupload';
 export class UploadFilesComponent implements OnInit {
   uploadedFiles: any[] = [];
   messages: Message[] = [];
-  
+
   constructor(
     private messageService: MessageService,
     private storage: Storage // Inicializar o Firebase Storage
   ) { }
-  
+
   ngOnInit() { }
 
   validatePdfFile(file: File): boolean {
     const fileName = file.name.toLowerCase();
-    
+
     // verificar se o arquivo é um PDF
     if (file.type !== 'application/pdf') {
+      this.messages = []
       this.messages.push({ severity: 'error', detail: 'O arquivo deve ser um PDF.' });
       return false;
     }
 
     // Verifica se o nome do arquivo começa com o que preciso
     if (!fileName.startsWith('empresa-holerite-mat')) {
+      this.messages = []
       this.messages.push({ severity: 'error', detail: 'O arquivo deve começar com "empresa-holerite-mat".' });
       return false;
     }
 
     return true;
   }
-  
-  uploadHandler(event: FileUploadHandlerEvent) {
+
+  async uploadHandler(event: FileUploadHandlerEvent) {
 
     for (const file of event.files) {
 
-      // validar o arquivo
       if (!this.validatePdfFile(file)) {
         continue; // Se não passar pela validação vai pular o up
       }
 
       this.uploadedFiles.push(file) // adicionar na lista
-      
+
       // criar uma referencia no storage
       const fileRef = ref(this.storage, file.name)
 
-      // fazer upload do arquivo
-      uploadBytes(fileRef, file).then((snapshot) => {
-        this.messages = []
-        this.messages.push({ 
-          severity: 'success',
-          detail: `${event.files.length === 1 ? 'Arquivo enviado' :  'Arquivos enviados'} com sucesso!` 
-        })
-      }).catch((error) => {
-        this.messages = []
-        this.messages.push({ 
-          severity: 'error', 
-          detail: `Erro ao enviar ${event.files.length === 1 ? 'o arquivo!' :  'os arquivos!'}` 
-        })
-      }),
-      // pega a URL
-      getDownloadURL(fileRef).then((url) => {
-        console.log(url)
-      })
-    }
+      // Fazer o upload com acompanhamento de progresso
+      const task = uploadBytesResumable(fileRef, file)
 
+      task.on('state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Progresso: ${progress}%`);
+
+        },
+        (_error) => {
+          this.messages.push({ severity: 'error', detail: `Erro ao enviar o arquivo: ${file.name}.` });
+        },
+        async () => {
+          // pegar a URL
+          const downloadURL = await getDownloadURL(task.snapshot.ref)
+          console.log(`Arquivo disponivel para download: ${downloadURL}`)
+          this.messages = []
+          this.messages.push({ severity: 'success', detail: `${event.files.length === 1 ? 'Arquivo enviado' :  'Arquivos enviados'} com sucesso!`});
+        }
+      )
+    }
     this.messages = this.messages ?? []
   }
 }
-
